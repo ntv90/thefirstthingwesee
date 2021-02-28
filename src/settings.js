b92cdd3ac37c3112f93a58a6626a072bf52f6d4e
VuNT <ntv90.work@gmail.com>
2021-02-28 23:34:24 +0700

    first commit

---
+++ src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/1_Scene/Loading.fire",orientation:"landscape",jsList:[]};
